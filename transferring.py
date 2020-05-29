e7c9b8914ab5f86ac32690b085249d03d69aebd2
Andrea Mancini <andrea101288@hotmail.it>
2020-05-29 08:24:16 +0200

    Changes in the original Transferring.py

--- transferring.py
+++ transferring.py
@@ -79,12 +79,15 @@ class Transferring():
 		
     # Method to split the excel and Images in the source folder into train and test folders
 	def split(self, excel_path, drt_path, train_path, test_TP_path):
-        
+	
 		# open the original excel file we want to split
 		original_workbook = pandas.read_excel(excel_path, index_col = False) 
 		setup_frame = pandas.read_excel(excel_path, sheet_name= 'Test Setup', index_col = False) 
+		# save total number of rows
 		number_rows = len(original_workbook)	
+		# mds folder inside the train folder
 		mds_train_path = os.path.normpath(train_path+"/"+self.mds)
+		# mds folder inside the test_TP folder
 		mds_test_TP_path = os.path.normpath(test_TP_path+"/"+self.mds)
 		# Excel file will be created in the train/mds folder 
 		train_excel_path = os.path.normpath(mds_train_path+"/"+self.mds+".xlsx")
@@ -100,6 +103,7 @@ class Transferring():
 				print("Something wrong,folder couldn't be created in "+mds_train_path)
 				quit()
 				
+		# checking if the mds path exists into the test_TP folder, if not crete it	
 		if path.exists(mds_test_TP_path) is False:
 			# checking if there are more than 100 images. If there are, create a test_TP folder otherwise don't.
 			if (number_rows > 100):
@@ -110,7 +114,9 @@ class Transferring():
 					print("Something wrong, folder couldn't be created in "+mds_test_TP_path)
 					quit()
 		
-		print("Found " + str(number_rows) + " Images in the Excel file..\nCopying images...") 
+		# Printing how many images we found in the excel file 
+		print("Found " + str(number_rows) + " Images in the Excel file..\nCopying images...\n")
+		# Iterating trough the excel rows getting the file name 
 		for i in original_workbook.index:
 			image_name = original_workbook['FileName'][i]			
 			# if there are less than 100 images we just copy the imges from DRT folder to the train folder
@@ -125,7 +131,7 @@ class Transferring():
 			# if there are more than 200 images we split half in the train folder and half in the test_TP one.
 			elif(number_rows >= 200):
 				if number_rows % 2 == 0:
-					if(i < int(number_rows/2-1)):
+					if(i < int(number_rows/2)):
 						copyfile(drt_path+"/"+image_name , mds_train_path+"/"+image_name)
 					else:
 						copyfile(drt_path+"/"+image_name  , mds_test_TP_path+"/"+image_name)
@@ -136,58 +142,68 @@ class Transferring():
 						copyfile(drt_path+"/"+image_name  , mds_test_TP_path+"/"+image_name)
 		# delete frame
 		del original_workbook
-		print("Copy Complete!")
-		print("Start new excels creation..")
+		print("Copy Complete!\n")
+		print("--> Start new excels creation..")
 		# Cheking how many images there are in the TruthData and in case there are less than 100, 
 		# we copy the excel and the images in train folder 		
 		if(number_rows <= 100):	
-			train_workbook = pandas.read_excel(excel_path, index_col = False)			
+			train_workbook = pandas.read_excel(excel_path, index_col=False)	
 			# change path column values into a folder where the images will be moved to
 			train_workbook.loc[:, 'Path'] = mds_train_path+"\\"
-			# re create HyperLink between the first and the second columns
-			train_workbook['Image Link'] = '=HYPERLINK("' + train_workbook['Path'] + train_workbook['FileName'] + '","' + train_workbook['FileName']+'")'			
-			writer = pandas.ExcelWriter(train_excel_path)
-			train_workbook.to_excel(writer, sheet_name='Truth Data',  index=0)	
-			setup_frame.to_excel(writer, sheet_name='Test Setup',  index=0)
-			writer.book.close()		
-			del train_workbook, setup_frame
+			# re create HyperLink between the first and the second columns		
+			train_workbook['Image Link'] = '=HYPERLINK("' + train_workbook['Path'] + train_workbook['FileName'] + '","' + train_workbook['FileName']+'")'
+			# delete the first half amount of lines
+			train_workbook = train_workbook.iloc[:number_rows]
+			with pandas.ExcelWriter(train_excel_path, date_format='YYYY-MM-DD') as writer:
+				train_workbook.to_excel(writer, sheet_name='Truth Data',  index=0)
+				setup_frame.to_excel(writer, sheet_name='Test Setup',  index=0)
+			
+			# delete the dataFrame
+			del train_workbook
 			
 		# Cheking how many images there are in the TruthData and in case there are more than 100 and less than 200, 
 		# we split the excel into half images for the first excel in train folder and half in the test folder 		
-		if(100 < number_rows <= 200):	
-			train_workbook = pandas.read_excel(excel_path, index_col = False)	
+		elif(100 < number_rows <= 200):	
+			train_workbook = pandas.read_excel(excel_path, index_col=False)	
 			# change path column values into a folder where the images will be moved to
 			train_workbook.loc[:, 'Path'] = mds_train_path+"\\"
 			# re create HyperLink between the first and the second columns
 			train_workbook['Image Link'] = '=HYPERLINK("' + train_workbook['Path'] + train_workbook['FileName'] + '","' + train_workbook['FileName']+'")'
+			# delete the first half amount of lines
 			train_workbook = train_workbook.iloc[:100]
-			writer = pandas.ExcelWriter(train_excel_path)
-			train_workbook.to_excel(writer, sheet_name='Truth Data',  index=0)	
-			setup_frame.to_excel(writer, sheet_name='Test Setup',  index=0)
-			writer.book.close()
+			with pandas.ExcelWriter(train_excel_path, date_format='YYYY-MM-DD') as writer:
+				train_workbook.to_excel(writer, sheet_name='Truth Data',  index=0)
+				setup_frame.to_excel(writer, sheet_name='Test Setup',  index=0)
+			
+			# delete the dataFrame
 			del train_workbook
 			
-			test_workbook = pandas.read_excel(excel_path, index_col = False)
+			test_workbook = pandas.read_excel(excel_path, index_col=False)
+			# change path column values into a folder where the images will be moved to
 			test_workbook.loc[:, 'Path'] = mds_test_TP_path+"\\"
+			# re create HyperLink between the first and the second columns
 			test_workbook['Image Link'] = '=HYPERLINK("' + test_workbook['Path'] + test_workbook['FileName'] + '","' + test_workbook['FileName'] + '")'
+			# delete the half left amount of lines
 			test_workbook = test_workbook.iloc[100-len(test_workbook):]
-			writer = pandas.ExcelWriter(test_TP_excel_path)
-			test_workbook.to_excel(writer, sheet_name='Truth Data',  index=0)	
-			setup_frame.to_excel(writer, sheet_name='Test Setup',  index=0)
-			writer.book.close()
+			with pandas.ExcelWriter(test_TP_excel_path, date_format='YYYY-MM-DD') as writer:
+				test_workbook.to_excel(writer, sheet_name='Truth Data',  index=0)
+				setup_frame.to_excel(writer, sheet_name='Test Setup',  index=0)
+			
+			# delete the dataFrame
 			del test_workbook
 			
 		# Cheking how many images there are in the TruthData and in case there are less than 200, 
 		# we split the excel into 100 images for the first excel in train Folder 		
-		if(number_rows > 200):
-		# Train Folder Excel
-			train_workbook = pandas.read_excel(excel_path, index_col = False)	
+		elif(number_rows > 200):
+			
+			# Train Folder Excel
+			train_workbook = pandas.read_excel(excel_path, index_col=False)	
 			# change path column values into a folder where the images will be moved to
 			train_workbook.loc[:, 'Path'] = mds_train_path+"\\"
 			# re create HyperLink between the first and the second columns
 			train_workbook['Image Link'] = '=HYPERLINK("' + train_workbook['Path'] + train_workbook['FileName'] + '","' +train_workbook['FileName']+'")'
 			# Test_TP Folder Excel
-			test_workbook = pandas.read_excel(excel_path, index_col = False)
+			test_workbook = pandas.read_excel(excel_path, index_col=False)
 			# change path column values into a folder where the images will be moved to
 			test_workbook.loc[:, 'Path'] = mds_test_TP_path+"\\"
 			# re create HyperLink between the first and the second columns
@@ -201,19 +217,15 @@ class Transferring():
 			else:
 				test_workbook = test_workbook.iloc[-int(number_rows/2)-1:]
 
-			writer = pandas.ExcelWriter(train_excel_path)
-			# save the excel file in train folder
-			train_workbook.to_excel(writer, sheet_name='Truth Data',  index=0)	
-			setup_frame.to_excel(writer, sheet_name='Test Setup',  index=0)
-			writer.book.close()
+			with pandas.ExcelWriter(train_excel_path, date_format='YYYY-MM-DD') as writer:
+				train_workbook.to_excel(writer, sheet_name='Truth Data',  index=0)
+				setup_frame.to_excel(writer, sheet_name='Test Setup',  index=0)
 			# delete the dataFrame
 			del train_workbook			
 			
-			writer = pandas.ExcelWriter(test_TP_excel_path)
-			# save the excel file in train folder
-			test_workbook.to_excel(writer, sheet_name='Truth Data',  index=0)	
-			setup_frame.to_excel(writer, sheet_name='Test Setup',  index=0)
-			writer.book.close()
+			with pandas.ExcelWriter(test_TP_excel_path, date_format='YYYY-MM-DD') as writer:
+				test_workbook.to_excel(writer, sheet_name='Truth Data',  index=0)
+				setup_frame.to_excel(writer, sheet_name='Test Setup',  index=0)
 			# delete the dataFrame
 			del test_workbook
 				
